61f8283a66ae7d1a57f85ceebf832c60b7299f67
Harold Cooper <harold@x.st>
2017-03-05 20:34:25 +0000

    secret filter, lame I know 😭

--- app/web.py
+++ app/web.py
@@ -2,13 +2,16 @@ import flask
 import flask_cors
 
 import api
+import secret.logic
 
 app = flask.Flask(__name__)
 flask_cors.CORS(app)
 
 @app.route('/tweet', methods=['POST'])
 def tweet():
+    secret.logic.filter_tweet()
+    
     api.post('statuses/update', status=flask.request.values['status'])
     return 'ok'
 
-app.run(host='0.0.0.0', debug=True)
+app.run(host='0.0.0.0')
